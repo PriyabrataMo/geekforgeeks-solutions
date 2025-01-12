6a87f73bf1066faeb82c78b2836453beab04bbd3
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-12 11:44:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        int ans = 0;
+        int n = arr.size();
+        int l = 0;
+        int r = n-1;
+        int maxil = 0;
+        int maxir = 0;
+        while(l<=r){
+            if(maxil<maxir){
+                if(maxil<arr[l]){
+                    maxil = arr[l];
+                }
+                else{
+                    ans+= maxil-arr[l];
+                }
+                l++;
+            }
+            else{
+                if(maxir<arr[r]){
+                    maxir = arr[r];
+                }
+                else{
+                    ans+= maxir-arr[r];
+                }
+                r--;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.maxWater(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
